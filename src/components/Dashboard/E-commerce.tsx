901b746d82b8182f74141d232a23286fbe484a78
Zakariae4AL <sarawatashi01@gmail.com>
2024-05-18 22:34:45 +0200

    Ajout usage reclamation v2

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -73,7 +73,7 @@ const ECommerce: React.FC = () => {
             />
           </svg>
         </CardDataStats>
-        <CardDataStats title="Total Users" total="3.456" rate="0.95%" levelDown>
+        <CardDataStats title="Total Users" total="3.456">
           <svg
             className="fill-primary dark:fill-white"
             width="22"
